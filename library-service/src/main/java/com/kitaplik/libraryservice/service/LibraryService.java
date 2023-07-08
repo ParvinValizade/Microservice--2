3fc70449a75c82114e3b652354155dd4eaa7d840
ParvinValizade <parvin.valizade@mail.ru>
2023-07-08 16:21:51 +0400

    grpc

--- library-service/src/main/java/com/kitaplik/libraryservice/service/LibraryService.java
+++ library-service/src/main/java/com/kitaplik/libraryservice/service/LibraryService.java
@@ -1,11 +1,15 @@
 package com.kitaplik.libraryservice.service;
 
+import com.kitaplik.bookservice.BookId;
+import com.kitaplik.bookservice.BookServiceGrpc;
+import com.kitaplik.bookservice.Isbn;
 import com.kitaplik.libraryservice.client.BookServiceClient;
 import com.kitaplik.libraryservice.dto.AddBookRequest;
 import com.kitaplik.libraryservice.dto.LibraryDto;
 import com.kitaplik.libraryservice.exception.LibraryNotFoundException;
 import com.kitaplik.libraryservice.model.Library;
 import com.kitaplik.libraryservice.repository.LibraryRepository;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,9 @@ public class LibraryService {
 
     private final LibraryRepository repository;
     private final BookServiceClient bookServiceClient;
+
+    @GrpcClient("book-service")
+    private BookServiceGrpc.BookServiceBlockingStub bookServiceBlockingStub;
 
     public LibraryService(LibraryRepository repository, BookServiceClient bookServiceClient) {
         this.repository = repository;
@@ -41,12 +48,13 @@ public class LibraryService {
     }
 
     public void addBookToLibrary(AddBookRequest request){
-        String bookId = bookServiceClient.getBookByIsbn(request.getIsbn()).getBody().getBookId();
+        BookId bookId = bookServiceBlockingStub.getBookIdByIsbn(Isbn.newBuilder().setIsbn(request.getIsbn()).build());
+//        String bookId = bookServiceClient.getBookByIsbn(request.getIsbn()).getBody().getBookId();
 
         Library library = repository.findById(request.getId())
                 .orElseThrow(()->new LibraryNotFoundException("Library could not found by id: "+ request.getId()));
    library.getUserBook()
-           .add(bookId);
+           .add(bookId.getBookId());
 
    repository.save(library);
     }
